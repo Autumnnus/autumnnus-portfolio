a163260511a04389a4f149b850820b270b96b0fb
Kadir <84664117+VectortheGreat@users.noreply.github.com>
2026-03-04 21:05:15 +0300

    security headers added

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,18 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 
+const securityHeaders = [
+  { key: "X-DNS-Prefetch-Control", value: "on" },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+  { key: "X-XSS-Protection", value: "1; mode=block" },
+  { key: "X-Frame-Options", value: "SAMEORIGIN" },
+  { key: "X-Content-Type-Options", value: "nosniff" },
+  { key: "Referrer-Policy", value: "origin-when-cross-origin" },
+];
+
 const nextConfig: NextConfig = {
   output: "standalone",
   images: {
@@ -29,6 +41,14 @@ const nextConfig: NextConfig = {
     serverActions: {
       bodySizeLimit: 10 * 1024 * 1024,
     },
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
   },
 };
 
